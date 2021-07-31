f7c6528d08f4b13776f3c8bc5fdd25411f8140f1
Lisandro Saez <lisandrosaezamelio@gmail.com>
2021-07-31 03:18:59 -0300

    sorting by index and name

--- segundo libro/acceso-datos.py
+++ segundo libro/acceso-datos.py
@@ -1,4 +1,6 @@
 import pandas as pd 
 pd.options.display.max_rows = 8
 data = pd.read_excel('AAPL.xlsx', sheet_name='Hoja1')
-print(data.loc[2])
+dataIndiceFecha = data.set_index("timestamp")
+print(dataIndiceFecha.loc["2020-03-04"])
+print(dataIndiceFecha.iloc[2])
